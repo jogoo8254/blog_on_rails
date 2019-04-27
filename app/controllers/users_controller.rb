16d8f142c5100e3dc695545be997b5458cc5be47
jogoo8254 <kev_inc88@hotmail.com>
2019-04-26 19:48:21 -0700

    implement edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    # before_action :user_params, only: [:update]
     def new
        @user = User.new 
     end
@@ -11,9 +12,21 @@ class UsersController < ApplicationController
             render :new
         end
     end
+    def edit
+        @user = User.find params[:id]
+        render :edit
+    end
+    def update
+        @user = User.find(params[:id])
+        if @user.update user_params
+            redirect_to edit_user_path(@user.id)
+        else
+            render :edit
+        end
+    end
     private
     def user_params
-        params.require(:user).permit(:name,:email,:password,:password_confirmation)
+        params.permit(:name,:email,:password,:password_confirmation)
     end
 
 end
